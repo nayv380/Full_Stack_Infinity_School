3bb486f43f1b7764ddceb45f35761b7def04372f
Nayara V <nay.380@icloud.com>
2025-09-06 16:07:45 -0300

    Add files via upload

---
+++ Aula 02/operadores_relacionais.py
@@ -0,0 +1,9 @@
+# Operadores Relacionais / Comparação
+
+print(2 == 2)            # == | Igualdade
+print('Nayara' == 'Nayara')  # == | Igualdade
+print('Nayara' != 'Pedro') # != | Diferente
+print(1 > 4)             # >  | Maior que
+print(4 >= 4)            # >= | Maior ou Igual a
+print(7 < 7)             # <  | Menor que
+print(7 <= 8)            # <= | Menor ou igual a
