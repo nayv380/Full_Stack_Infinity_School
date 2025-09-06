12ee6efc520d7f826b4bb85eb17686d7a121c475
Nayara V <nay.380@icloud.com>
2025-09-06 16:15:24 -0300

    Delete Logica_de_Programação directory

--- Logica_de_Programação/ex01.py
+++
@@ -1,7 +0,0 @@
-print('----- SISTEMA DETRAN MG -----')
-idade = int(input('Digite a sua idade: '))
-
-if idade >= 18:
-    print('Você pode tirar a carteira.')
-else:
-    print('Você não pode tirar a carteira.')
--- Logica_de_Programação/ex04.py
+++
@@ -1,9 +0,0 @@
-idade = int(input('Digite sua idade: '))
-
-
-if idade < 16:
-    print('Não pode votar.')
-elif (idade >= 16 and idade < 18) or idade >= 65:
-    print('Voto Facultativo.')
-else:
-    print('Voto Obrigatório.')
